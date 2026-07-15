23b2113345a86f391eb1899512fe5c0e308f0da9
Sanskriti Mittal <sanskritimittal199@gmail.com>
2026-07-15 15:08:03 +0530

    Time: 0 ms (100%), Space: 10.2 MB (24.75%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool check(int i, string &s) {
+    int n = s.length();
+
+    if (i >= n / 2)
+    return true;
+
+    if (s[i] != s[n - i - 1])
+    return false;
+
+    return check(i + 1, s);
+    }
+
+    bool isPalindrome(string s) {
+    string t;
+
+    for (char c : s) {
+    if (isalnum(c))
+    t += tolower(c);
+        }
+
+    return check(0, t);
+    }
+};
