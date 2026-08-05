1a404eedb4c402415921d3e63c3a9d8e59fc537b
Sanskriti Mittal <sanskritimittal199@gmail.com>
2026-08-05 23:48:44 +0530

    Time: 0 ms (100%), Space: 52.4 MB (25.52%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int max_reachable = 0;
+        int n = nums.size();
+        
+        for (int i = 0; i < n; ++i) {
+            if (i > max_reachable) {
+                return false;
+            }
+            max_reachable = max(max_reachable, i + nums[i]);
+            if (max_reachable >= n - 1) {
+                return true;
+            }
+        }
+        
+        return true;
+    }
+};
