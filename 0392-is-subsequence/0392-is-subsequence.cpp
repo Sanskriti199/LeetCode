53becc41a17d3187ed6f3bb0bb67484c9af05135
Sanskriti Mittal <sanskritimittal199@gmail.com>
2026-08-20 23:04:57 +0530

    Time: 0 ms (100%), Space: 8.7 MB (32.45%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
+    //two pointers
      int i=0;
      int j=0;
 
+    //storing the lengths
      int n=s.length();
      int m=t.length();
 
